a29152fcaf64017cb24c89a32fa2366eeec99a38
Felix Preda <felixpreda@yahoo.com>
2017-10-31 01:57:37 +0200

    Updates

--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+var secret = require('../config').secret;
 
 var UserSchema = new mongoose.Schema({
     username: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true },
@@ -11,5 +14,37 @@ var UserSchema = new mongoose.Schema({
 }, { timestap: true });
 
 UserSchema.plugin(uniqueValidator, { message: 'is already taken' });
+
+UserSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = function() {
+    var today = new Date();
+    var exp = new Date(today);
+    exp.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+        id: this._id,
+        username: this.username,
+        exp: parseInt(exp.getTime() / 1000),
+    }, secret);
+};
+
+UserSchema.methods.toAuthJSON = function() {
+    return {
+        username: this.username,
+        email: this.email,
+        token: this.generateJWT(),
+        bio: this.bio,
+        image: this.image
+    };
+};
 
 mongoose.model('User', UserSchema);
